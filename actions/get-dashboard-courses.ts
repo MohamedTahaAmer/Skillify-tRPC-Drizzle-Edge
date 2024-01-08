5d255c7a938e2b9f9910c597f36608412ea6d628
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-01-08 22:05:17 +0200

    chore: :ambulance: run npx prettier --w .

--- actions/get-dashboard-courses.ts
+++ actions/get-dashboard-courses.ts
@@ -1,58 +1,64 @@
-import { Category, Chapter, Course } from "@prisma/client";
+import { Category, Chapter, Course } from "@prisma/client"
 
-import { db } from "@/lib/db";
-import { getProgress } from "@/actions/get-progress";
+import { db } from "@/lib/db"
+import { getProgress } from "@/actions/get-progress"
 
 type CourseWithProgressWithCategory = Course & {
-  category: Category;
-  chapters: Chapter[];
-  progress: number | null;
-};
-
-type DashboardCourses = {
-  completedCourses: CourseWithProgressWithCategory[];
-  coursesInProgress: CourseWithProgressWithCategory[];
+	category: Category
+	chapters: Chapter[]
+	progress: number | null
 }
 
-export const getDashboardCourses = async (userId: string): Promise<DashboardCourses> => {
-  try {
-    const purchasedCourses = await db.purchase.findMany({
-      where: {
-        userId: userId,
-      },
-      select: {
-        course: {
-          include: {
-            category: true,
-            chapters: {
-              where: {
-                isPublished: true,
-              }
-            }
-          }
-        }
-      }
-    });
+type DashboardCourses = {
+	completedCourses: CourseWithProgressWithCategory[]
+	coursesInProgress: CourseWithProgressWithCategory[]
+}
 
-    const courses = purchasedCourses.map((purchase) => purchase.course) as CourseWithProgressWithCategory[];
+export const getDashboardCourses = async (
+	userId: string,
+): Promise<DashboardCourses> => {
+	try {
+		const purchasedCourses = await db.purchase.findMany({
+			where: {
+				userId: userId,
+			},
+			select: {
+				course: {
+					include: {
+						category: true,
+						chapters: {
+							where: {
+								isPublished: true,
+							},
+						},
+					},
+				},
+			},
+		})
 
-    for (let course of courses) {
-      const progress = await getProgress(userId, course.id);
-      course["progress"] = progress;
-    }
+		const courses = purchasedCourses.map(
+			(purchase) => purchase.course,
+		) as CourseWithProgressWithCategory[]
 
-    const completedCourses = courses.filter((course) => course.progress === 100);
-    const coursesInProgress = courses.filter((course) => (course.progress ?? 0) < 100);
+		for (let course of courses) {
+			const progress = await getProgress(userId, course.id)
+			course["progress"] = progress
+		}
 
-    return {
-      completedCourses,
-      coursesInProgress,
-    }
-  } catch (error) {
-    console.log("[GET_DASHBOARD_COURSES]", error);
-    return {
-      completedCourses: [],
-      coursesInProgress: [],
-    }
-  }
+		const completedCourses = courses.filter((course) => course.progress === 100)
+		const coursesInProgress = courses.filter(
+			(course) => (course.progress ?? 0) < 100,
+		)
+
+		return {
+			completedCourses,
+			coursesInProgress,
+		}
+	} catch (error) {
+		console.log("[GET_DASHBOARD_COURSES]", error)
+		return {
+			completedCourses: [],
+			coursesInProgress: [],
+		}
+	}
 }
--- app/api/courses/[courseId]/attachments/route.ts
+++ app/api/courses/[courseId]/attachments/route.ts
@@ -1,42 +1,42 @@
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
 
-import { db } from "@/lib/db";
+import { db } from "@/lib/db"
 
 export async function POST(
-  req: Request,
-  { params }: { params: { courseId: string } }
+	req: Request,
+	{ params }: { params: { courseId: string } },
 ) {
-  try {
-    const { userId } = auth();
-    const { url } = await req.json();
+	try {
+		const { userId } = auth()
+		const { url } = await req.json()
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 })
+		}
 
-    const courseOwner = await db.course.findUnique({
-      where: {
-        id: params.courseId,
-        userId: userId,
-      }
-    });
+		const courseOwner = await db.course.findUnique({
+			where: {
+				id: params.courseId,
+				userId: userId,
+			},
+		})
 
-    if (!courseOwner) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+		if (!courseOwner) {
+			return new NextResponse("Unauthorized", { status: 401 })
+		}
 
-    const attachment = await db.attachment.create({
-      data: {
-        url,
-        name: url.split("/").pop(),
-        courseId: params.courseId,
-      }
-    });
+		const attachment = await db.attachment.create({
+			data: {
+				url,
+				name: url.split("/").pop(),
+				courseId: params.courseId,
+			},
+		})
 
-    return NextResponse.json(attachment);
-  } catch (error) {
-    console.log("COURSE_ID_ATTACHMENTS", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
+		return NextResponse.json(attachment)
+	} catch (error) {
+		console.log("COURSE_ID_ATTACHMENTS", error)
+		return new NextResponse("Internal Error", { status: 500 })
+	}
 }
