b31465eaf49ceb63ce323455c1e35f1a107ea24a
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-01-08 20:17:23 +0200

    init

---
+++ ZNotes.ts
@@ -0,0 +1,13 @@
+//#region // < Build a Course & LMS Platform: Next 13, React, Stripe, Mux, Prisma, Tailwind, MySQL | Udemy Clone
+/*
+1- the prgress bar
+2- the navigation side bar with background and an indicator stripe
+3- the alart banner
+4- how to track the video watch time progress
+5- confitty animation
+6- drag and drop reordering
+7- rich text editor
+8- multi step form
+9- image upload with upload thing, also these is video and files upload
+*/
+//#endregion
