4e570a69d881783f36b75941d2acaa3f7ae0d296
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-01-11 06:52:15 +0200

    fix: :bug: Fix the cumulative layout shift on the chapters list

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -37,11 +37,47 @@ export const ChaptersList = ({
 	// }, [items])
 
 	if (!isMounted) {
-		return null
+		return (
+			<div>
+				{chapters.map((chapter) => (
+					<div
+						key={chapter.id}
+						className={cn(
+							"flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
+							chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-700"
+						)}
+					>
+						<div
+							className={cn(
+								"px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
+								chapter.isPublished && "border-r-sky-200 hover:bg-sky-200"
+							)}
+						>
+							<Grip className="h-5 w-5" />
+						</div>
+						{chapter.title}
+						<div className="ml-auto pr-2 flex items-center gap-x-2">
+							{chapter.isFree && <Badge>Free</Badge>}
+							<Badge
+								className={cn(
+									"bg-slate-500",
+									chapter.isPublished && "bg-sky-700"
+								)}
+							>
+								{chapter.isPublished ? "Published" : "Draft"}
+							</Badge>
+							<Pencil
+								onClick={() => onEdit(chapter.id)}
+								className="w-4 h-4 cursor-pointer hover:opacity-75 transition"
+							/>
+						</div>
+					</div>
+				))}
+			</div>
+		)
 	}
 
 	const onDragEnd = (result: DropResult) => {
-		console.log("Expression")
 		if (!result.destination) return
 
 		const items = Array.from(chapters)
