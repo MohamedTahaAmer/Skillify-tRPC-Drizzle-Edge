a0a36415ef2a894e2d30395012478889877d8e35
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-02-05 16:21:10 +0200

    style: :art: truncate the courses titles

--- src/app/test/edge/page.tsx
+++ src/app/test/edge/page.tsx
@@ -3,8 +3,19 @@ export const runtime = "edge"
 const Page = () => {
 	return (
 		<>
-			<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-				page - edge
+			<div className="mx-auto flex w-72 flex-col pt-8">
+				<div className="truncate">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+					dolorem cupiditate placeat autem asperiores porro perspiciatis amet
+					est sint a? Placeat voluptatem reiciendis, fugit architecto velit unde
+					quibusdam rem consequatur!
+				</div>
+				<div className="line-clamp-6">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+					dignissimos velit eius, blanditiis doloremque modi optio consequuntur
+					soluta voluptas aliquid deleniti incidunt pariatur, perferendis
+					obcaecati quis aliquam illo. Cumque, autem!
+				</div>
 			</div>
 		</>
 	)
--- src/components/course-card.tsx
+++ src/components/course-card.tsx
@@ -32,7 +32,7 @@ export const CourseCard = ({
 					<Image fill className="object-cover" alt={title} src={imageUrl} />
 				</div>
 				<div className="flex flex-col pt-2">
-					<div className="line-clamp-2 text-lg font-medium transition group-hover:text-sky-700 md:text-base">
+					<div className="truncate text-lg font-medium transition group-hover:text-sky-700 sm:text-base">
 						{title}
 					</div>
 					<p className="text-xs text-muted-foreground">{category}</p>
