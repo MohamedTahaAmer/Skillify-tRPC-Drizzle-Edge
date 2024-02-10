074d35bfafbc2774ce4380c8b0daec7a33027481
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-02-10 07:22:11 +0200

    test: :bug: Test r-test/home-page, r-test/courses-num worked, but test/courses-num didn't although it's the same page,  but different layouts
    Also testing supensing the usePath in the navbar, in the layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import { ToastProvider } from "@/components/providers/toaster-provider"
 import { TRPCReactProvider } from "@/trpc/react"
 import ClientClerkProvider from "./_components/client-clerk-provider"
 import { Navbar } from "./_components/navbar"
+import { Suspense } from "react"
 
 export default function RootLayout({
 	children,
@@ -16,7 +17,9 @@ export default function RootLayout({
 			<TRPCReactProvider>
 				<div className="fixed inset-x-0 top-0 z-10 h-14 w-full">
 					<ClientClerkProvider>
-						<Navbar />
+						<Suspense fallback={null}>
+							<Navbar />
+						</Suspense>
 					</ClientClerkProvider>
 				</div>
 				<div className="pt-14">{children}</div>
---
+++ src/app/r-test/home-page/page.tsx
@@ -0,0 +1,58 @@
+import { SearchInput } from "@/components/search-input"
+
+import { CoursesList } from "@/components/courses-list"
+import { env } from "@/env"
+import type {
+	CategoriesSelect,
+	ChaptersSelect,
+	CoursesSelect,
+} from "@/server/db/schema"
+import uniqBy from "lodash.uniqby"
+import { Suspense } from "react"
+import { Categories } from "@/app/(root)/_components/categories"
+import ShowProgressInfoCards from "@/app/(root)/_components/show-progress-info-cards"
+
+const HomePage = async () => {
+	type Courses = (CoursesSelect & {
+		category: CategoriesSelect | null
+		chapters: { id: ChaptersSelect["id"] }[]
+	})[]
+	console.log(`${env.NEXT_PUBLIC_APP_URL}/api/trpc/get.getAllPublishedCourses`)
+	let res = await fetch(
+		`${env.NEXT_PUBLIC_APP_URL}/api/trpc/get.getAllPublishedCourses`,
+	)
+	if (!res.ok) {
+		console.log("Failed to fetch courses")
+		return
+	}
+	let { result } = (await res.json()) as {
+		result: { data: { json: Courses } }
+	}
+	let allCourses = result.data.json
+
+	let categories = allCourses.map((course) => course.category)
+	categories = uniqBy(categories, (c) => c?.name)
+
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			{/* search input */}
+			<div className="fixed left-1/2 top-2  z-20 block  w-[65%] -translate-x-1/2 px-6 md:w-1/2 ">
+				<SearchInput />
+			</div>
+
+			{/* main Content */}
+			<div className="container space-y-4 py-4 lg:px-6">
+				{/* category, purchased and clear badges */}
+				<Categories items={categories} />
+
+				{/* progress info cards */}
+				{allCourses.length > 0 && <ShowProgressInfoCards />}
+
+				{/* courses list */}
+				<CoursesList items={allCourses} />
+			</div>
+		</Suspense>
+	)
+}
+
+export default HomePage
