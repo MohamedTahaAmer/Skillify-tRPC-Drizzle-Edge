17f94e5b9bda3b6a232cce9b4d1da10ddd6f27e1
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-02-10 07:43:52 +0200

    test: :bug: suspensed-cleint-clerk

---
+++ src/app/r-test/suspensed-cleint-clerk/page.tsx
@@ -0,0 +1,70 @@
+import { SearchInput } from "@/components/search-input"
+
+import { CoursesList } from "@/components/courses-list"
+import { env } from "@/env"
+import type {
+	CategoriesSelect,
+	ChaptersSelect,
+	CoursesSelect,
+} from "@/server/db/schema"
+import uniqBy from "lodash.uniqby"
+import { Suspense } from "react"
+import { Categories } from "@/app/(root)/_components/categories"
+import ShowProgressInfoCards from "@/app/(root)/_components/show-progress-info-cards"
+import { TRPCReactProvider } from "@/trpc/react"
+import { Navbar } from "@/app/(root)/_components/navbar"
+import ClientClerkProvider from "@/app/(root)/_components/client-clerk-provider"
+
+const HomePage = async () => {
+	type Courses = (CoursesSelect & {
+		category: CategoriesSelect | null
+		chapters: { id: ChaptersSelect["id"] }[]
+	})[]
+	console.log(`${env.NEXT_PUBLIC_APP_URL}/api/trpc/get.getAllPublishedCourses`)
+	let res = await fetch(
+		`${env.NEXT_PUBLIC_APP_URL}/api/trpc/get.getAllPublishedCourses`,
+	)
+	if (!res.ok) {
+		console.log("Failed to fetch courses")
+		return
+	}
+	let { result } = (await res.json()) as {
+		result: { data: { json: Courses } }
+	}
+	let allCourses = result.data.json
+
+	let categories = allCourses.map((course) => course.category)
+	categories = uniqBy(categories, (c) => c?.name)
+
+	return (
+		<TRPCReactProvider>
+			<div className="fixed inset-x-0 top-0 z-10 h-14 w-full">
+				<Suspense fallback={<div>Loading...</div>}>
+					<ClientClerkProvider>
+						<Navbar />
+					</ClientClerkProvider>
+				</Suspense>
+			</div>
+			<Suspense fallback={<div>Loading...</div>}>
+				{/* search input */}
+				<div className="fixed left-1/2 top-2  z-20 block  w-[65%] -translate-x-1/2 px-6 md:w-1/2 ">
+					<SearchInput />
+				</div>
+
+				{/* main Content */}
+				<div className="container space-y-4 py-4 pt-14 lg:px-6">
+					{/* category, purchased and clear badges */}
+					<Categories items={categories} />
+
+					{/* progress info cards */}
+					{allCourses.length > 0 && <ShowProgressInfoCards />}
+
+					{/* courses list */}
+					<CoursesList items={allCourses} />
+				</div>
+			</Suspense>
+		</TRPCReactProvider>
+	)
+}
+
+export default HomePage
