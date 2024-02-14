bd024e62388b38ad2a268985ca15425a796f389d
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-02-15 00:07:17 +0200

    feat: :ambulance: This is the correct way for absolute imports in Vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
 	plugins: [react()],
@@ -9,7 +8,7 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "src"),
+			"@": "/src",
 		},
 	},
 })
